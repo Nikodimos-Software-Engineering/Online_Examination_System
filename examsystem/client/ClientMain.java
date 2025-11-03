f18c7a38216d7d90825e137bf6a1b0508d311e67
= <=>
2025-11-03 10:32:07 -0800

    Adding All Project Files

---
+++ examsystem/client/ClientMain.java
@@ -0,0 +1,28 @@
+package client;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+
+public class ClientMain {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 12345;
+    private static Socket socket;
+    private static BufferedReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+        try {
+            
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            SwingUtilities.invokeLater(() -> new LoginUI(socket, in, out));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to connect to server: " + e.getMessage(),
+                    "Connection Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+}
